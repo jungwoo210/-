228e7f26ea072f7ccc62ba0afc329946175d3469
jungwoo210 <103185497+jungwoo210@users.noreply.github.com>
2022-04-09 23:12:23 +0900

    2495 백준
    please 팔로우

---
+++ 백준.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+char number[8];
+int numbernumber[8];
+int max = -1;
+int maxmax = 0;
+int check(int a) {
+	maxmax = 0;
+	for (int g = 0; g <= 8; g++) {
+		if (number[a] == number[a + g])
+		{
+			maxmax++;
+		}
+		else {
+			break;
+		}
+	}
+	return maxmax;
+
+
+}
+int main() {
+	for (int i = 0; i < 3; i++) {
+		max = -1;
+		scanf("%s", number);
+		for (int j = 0; j < 8; j++) {
+			if (check(j) > max) { max = check(j); }
+
+		}
+		printf("%d \n", max);
+	}
+
+
+}
